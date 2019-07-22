2e36f7a30f28c192a3fdc6d9f729d151f92e906a
Zhanibek <zhanikzekeshov@gmail.com>
2019-07-22 12:28:21 +0600

    Update App.jsx

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -19,15 +19,17 @@ const App = () => {
   const lastChar = s => !isNaN(Number(s[s.length - 1]))
 
   const renderCommitList = () => commits.map(({hash, author, commit}) => {
-    const isNumber = 
-  } (
+    const isNumber = lastChar(hash)
+    
+    return (
     <li key={hash} style={{
       listStyleType: 'none', 
-      backgroundColor: lastChar(hash) && '#ffff00'}}>
+      backgroundColor: isNumber && '#ffff00'}}>
         <h2>{author}</h2>
         <p style={{marginLeft: 50}}>{commit}</p>
     </li>
-  ))
+  )
+  })
 
   return loading ? <span>Content is loading...</span> : (
     <div>
